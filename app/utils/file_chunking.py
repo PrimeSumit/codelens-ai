7dee8bb7dd6f4662fccf7b0f6f96b420c9ca42be
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-07-22 22:09:36 +0530

    chunk of py,markdown,json added

--- app/utils/file_chunking.py
+++ app/utils/file_chunking.py
@@ -1,6 +1,7 @@
-import ast
+import ast,json
 from pathlib import Path
 from app.utils.file_reader import read_file
+
 def chunk_python(file_path: Path):
     source_code=read_file(file_path)
     if not source_code:
@@ -11,22 +12,100 @@ def chunk_python(file_path: Path):
         return []
 
     chunks = []
-
+    lines = source_code.splitlines()
     for node in tree.body:
         if isinstance(node,ast.FunctionDef):
-            pass
+            code = "\n".join(lines[node.lineno - 1 : node.end_lineno])
+
+            chunks.append({
+                "file_path": str(file_path),
+                "type": "function",
+                "name": node.name,
+                "start_line": node.lineno,
+                "end_line": node.end_lineno,
+                "code": code,
+            })
         elif isinstance(node,ast.AsyncFunctionDef):
-            pass
+            code = "\n".join(lines[node.lineno - 1 : node.end_lineno])
+
+            chunks.append({
+                "file_path": str(file_path),
+                "type": "async_function",
+                "name": node.name,
+                "start_line": node.lineno,
+                "end_line": node.end_lineno,
+                "code": code,
+            })
+        elif isinstance(node,ast.ClassDef):
+            code = "\n".join(lines[node.lineno - 1 : node.end_lineno])
+
+            chunks.append({
+                "file_path": str(file_path),
+                "type": "class",
+                "name": node.name,
+                "start_line": node.lineno,
+                "end_line": node.end_lineno,
+                "code": code,
+            })
+        
 
     return chunks
 
 
 def chunk_markdown(file_path: Path):
-    pass
+    readme=read_file(file_path)
+    if not readme:
+        return []
+    lines=readme.splitlines()
+    has_heading = any(line.lstrip().startswith("#") for line in lines)
+    if not has_heading:
+        return [{
+            "file_path":str(file_path),
+            "type":"markdown",
+            "name":file_path.stem,
+            "code":readme
+        }]
+    current_chunk=[]
+    
+    chunks=[]
 
+    for line in lines:
+        if line.lstrip().startswith('#'):
+            if current_chunk:
+                chunks.append(current_chunk)
+            current_chunk=[line]
+        else:
+            current_chunk.append(line)
+    if current_chunk:
+        chunks.append(current_chunk)
+    result=[]
+    for chunk in chunks:
+        title = chunk[0].lstrip("#").strip()
+        code = "\n".join(chunk)
+        result.append({
+                "file_path": str(file_path),
+                "type": "markdown",
+                "name": title,
+                "code": code,
+            })
+    return result
 
 def chunk_json(file_path: Path):
-    pass
+    content=read_file(file_path)
+    if not content:
+        return []
+    try:
+        json.loads(content)
+    except json.JSONDecodeError:
+        return []
+    return [{
+        "file_path": str(file_path),
+        "type": "json",
+        "name": file_path.stem,
+        "code": content
+    }]
+
+    
 
 
 def chunk_yaml(file_path: Path):
