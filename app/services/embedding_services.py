81f4586c61df63622d8e1223742f5e3c18628733
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-07-28 21:41:36 +0530

    feat: implement passage and query for embedding chunks

--- app/services/embedding_services.py
+++ app/services/embedding_services.py
@@ -5,7 +5,7 @@ class EmbeddingServices:
     def __init__(self):
         self.client=OpenAI(api_key=settings.NVIDIA_API_KEY,base_url="https://integrate.api.nvidia.com/v1")
 
-    def passage(self,texts:list[str]):
+    def passage(self,texts:list[str])->list[list[float]]:
         response=self.client.embeddings.create(
             input=texts,
             model=settings.EMBEDDING_MODEL,
@@ -17,5 +17,15 @@ class EmbeddingServices:
         )
         return [item.embedding for item in response.data]
 
-    def query(self,text:str):
-        pass
+    def query(self,text:str)->list[float]:
+        response=self.client.embeddings.create(
+            input=[text],
+            model=settings.EMBEDDING_MODEL,
+            encoding_format="float",
+            extra_body={
+                "input_type": "query",
+                "truncate": "NONE",
+            },
+
+        )
+        return response.data[0].embedding
