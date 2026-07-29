6188a1cab26783216773f7138ebc515834162d67
Sumit Ranjan Bahalia <165286043+PrimeSumit@users.noreply.github.com>
2026-07-29 19:02:26 +0530

    feat: store in qdrant

--- app/services/qdrant_service.py
+++ app/services/qdrant_service.py
@@ -1,7 +1,7 @@
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance,VectorParams
+from qdrant_client.models import Distance,VectorParams,PointStruct
 from app.core.config import settings
-
+from uuid import uuid4
 class QdrantService:
     def __init__(self):
         self.client=QdrantClient(
@@ -22,3 +22,28 @@ class QdrantService:
             )
         )
         return True
+
+    def upsert_chunks(self,repository_id:int,chunks:list[dict],embeddings:list[list[float]]):
+        if len(chunks)!=len(embeddings):
+            raise ValueError("Number of chunks and embeddings must be equal.")
+
+        points=[]
+        for chunk,embedding in zip(chunks,embeddings):
+            point=PointStruct(
+                id=str(uuid4()),
+                vector=embedding,
+                payload={
+                    "repository_id": repository_id,
+                    "file_path": chunk["file_path"],
+                    "type": chunk["type"],
+                    "name": chunk["name"],
+                    "start_line": chunk["start_line"],
+                    "end_line": chunk["end_line"],
+                    "code": chunk["code"]
+                }
+                
+            )
+            points.append(point)
+        self.client.upsert(collection_name=settings.QDRANT_COLLECTION,
+                               points=points)
+        return len(points)
